4f7bbcec3b71388f007f6043d17d3c888d06c791
yuya <ytennissunday@icloud.com>
2022-01-24 16:50:12 +0900

    text6

---
+++ src/component/tick.js
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom';
+
+function Clock(props){
+  return(
+    <div> 
+     <h1> Hello</h1>
+     <h2> It is {props.date.toLocaleTimeString()} . </h2>
+    </div>
+  );
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
+export default Clock 
